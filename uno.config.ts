278c6ebd08184966451e82dad92da58fb3b839c7
aibayanyu20 <aibayanyu@qq.com>
2023-09-12 10:45:50 +0800

    chore: init project

---
+++ uno.config.ts
@@ -0,0 +1,36 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetUno,
+  presetWebFonts,
+  transformerDirectives,
+  transformerVariantGroup
+} from 'unocss'
+
+export default defineConfig({
+  safelist: [],
+  theme: {},
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetTypography(),
+    presetIcons({
+      scale: 1.2,
+      warn: true
+    }),
+    presetWebFonts({
+      fonts: {
+        sans: 'DM Sans',
+        serif: 'DM Serif Display',
+        mono: 'DM Mono'
+      }
+    })
+  ],
+  shortcuts: [
+    ['flex-center', 'flex items-center justify-center'],
+    ['flex-between', 'flex items-center justify-between']
+  ],
+  transformers: [transformerDirectives(), transformerVariantGroup()]
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import Markdown from 'unplugin-vue-markdown/vite'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import AutoProps from 'unplugin-vue-tsx-auto-props/vite'
+import Unocss from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      include: [/\.vue$/, /\.md$/]
+    }),
+    vueJsx(),
+    AutoProps(),
+    Markdown({}),
+    // should be placed after `Markdown()`
+    Components({
+      // allow auto load markdown components under `./src/components/`
+      extensions: ['vue', 'md'],
+      dts: 'types/components.d.ts'
+
+      // allow auto import and register components used in markdown
+      // customLoaderMatcher: (path) => path.endsWith('.md')
+    }),
+    AutoImport({
+      imports: ['vue', 'pinia', 'vue-router', 'vue-i18n', '@vueuse/core'],
+      dts: 'types/auto-imports.d.ts'
+    }),
+    Unocss()
+  ]
 })
